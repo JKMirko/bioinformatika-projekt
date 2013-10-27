37ccda7f0e1267f99deeaca1b6775368d98da8c4
Mirko Jurić-Kavelj <mirko.juric-kavelj@fer.hr>
2013-10-27 23:40:51 +0100

    Najbolji commit ikad!

---
+++ src/najbolji/projekt/ikad/NajboljaKlasaIkad.java
@@ -0,0 +1,9 @@
+package najbolji.projekt.ikad;
+
+public class NajboljaKlasaIkad {
+
+	public static void main(String[] args) {
+		System.out.println("Najbolja ekipa ikad za najbolji projekt ikad!");
+	}
+
+}
